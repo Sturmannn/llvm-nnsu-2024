d518fac14dc064463492aa41bc40e235583ce597
Sturmannn <vladimir-ych@yandex.ru>
2024-03-17 01:08:17 +0300

    Done

--- clang/labs/lab1/durandin_vladimir/AlwaysInlineAttributePlugin.cpp
+++ clang/labs/lab1/durandin_vladimir/AlwaysInlineAttributePlugin.cpp
@@ -3,95 +3,66 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 
-using namespace clang;
-using namespace llvm;
-
-class AddAlwaysInlineConsumer : public ASTConsumer {
+class AddAlwaysInlineConsumer : public clang::ASTConsumer {
 public:
+  bool HasCondition(clang::Stmt *S) {
+    if (!S) {
+      return false;
+    }
 
+    if (llvm::isa<clang::IfStmt>(S) || llvm::isa<clang::SwitchStmt>(S) ||
+        llvm::isa<clang::WhileStmt>(S) || llvm::isa<clang::DoStmt>(S) ||
+        llvm::isa<clang::ForStmt>(S)) {
+      return true;
+    }
 
-bool HasCondition(clang::Stmt *S) {
-  if (!S) {
+    for (clang::Stmt *Child : S->children()) {
+      if (HasCondition(Child)) {
+        return true;
+      }
+    }
+
     return false;
   }
 
-  // Проверка наличия условий на текущем уровне
-  if (isa<IfStmt>(S) || isa<SwitchStmt>(S) || isa<WhileStmt>(S) ||
-      isa<DoStmt>(S) || isa<ForStmt>(S)) {
-    return true;
-  }
-
-  // Рекурсивный поиск условий в дочерних элементах
-  for (Stmt *Child : S->children()) {
-    if (HasCondition(Child)) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-  bool HandleTopLevelDecl(DeclGroupRef D) override {
+  bool HandleTopLevelDecl(clang::DeclGroupRef D) override {
     clang::FunctionDecl *FD = nullptr;
-    for (Decl *Decl : D)
-    {
-      if (FD = dyn_cast<FunctionDecl>(Decl)) {
+
+    for (clang::Decl *Decl : D) {
+      FD = clang::dyn_cast<clang::FunctionDecl>(Decl);
+      if (FD) {
         if (!HasCondition(FD->getBody())) {
-          FD->addAttr(clang::AlwaysInlineAttr::CreateImplicit(FD->getASTContext()));
+          FD->addAttr(
+              clang::AlwaysInlineAttr::CreateImplicit(FD->getASTContext()));
           FD->print(llvm::outs() << "__attribute__((always_inline)) ");
-        }
-        else FD->print(llvm::outs());
+        } else
+          FD->print(llvm::outs());
       }
     }
     return true;
   }
 };
 
-class AddAlwaysInlineAction : public PluginASTAction {
+class AddAlwaysInlineAction : public clang::PluginASTAction {
 protected:
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &Compiler,
-                                                 llvm::StringRef InFile) override {
+  std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance &Compiler,
+                    llvm::StringRef InFile) override {
     return std::make_unique<AddAlwaysInlineConsumer>();
   }
-  
-  bool ParseArgs(const CompilerInstance &CI, const std::vector<std::string> &args) override {
+
+  bool ParseArgs(const clang::CompilerInstance &CI,
+                 const std::vector<std::string> &args) override {
+    for (const std::string &arg : args) {
+      if (arg == "--help") {
+        llvm::outs() << "This plugin adds the always_inline attribute to "
+                        "functions if they do not have conditions!\n";
+        return false;
+      }
+    }
     return true;
   }
 };
 
-static clang::FrontendPluginRegistry::Add<AddAlwaysInlineAction> X("AddAlwaysInline",
-                                                               "Adds always_inline to functions without conditions");
-
-
-
-// #include "clang/Frontend/FrontendPluginRegistry.h"
-// #include "clang/Frontend/CompilerInstance.h"
-// #include "clang/AST/ASTConsumer.h"
-// #include "clang/AST/ASTContext.h"
-// #include "clang/AST/RecursiveASTVisitor.h"
-// #include "llvm/Support/raw_ostream.h"
-
-// using namespace clang;
-
-// class ASTDumpConsumer : public ASTConsumer {
-// public:
-//     void HandleTranslationUnit(ASTContext &Context) override {
-//         llvm::outs() << "AST Dump:\n";
-//         Context.getTranslationUnitDecl()->dump();
-//     }
-// };
-
-// class ASTDumpAction : public PluginASTAction {
-// protected:
-//     std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, llvm::StringRef) override {
-//         return std::make_unique<ASTDumpConsumer>();
-//     }
-
-//     bool ParseArgs(const CompilerInstance &CI, const std::vector<std::string> &args) override {
-//         return true;
-//     }
-// };
-
-// static FrontendPluginRegistry::Add<ASTDumpAction>
-// X("assst-dump", "Dump the AST of the input code");
-
+static clang::FrontendPluginRegistry::Add<AddAlwaysInlineAction>
+    X("AddAlwaysInline", "Adds always_inline to functions without conditions");
--- clang/test/lab1/durandin_vladimir/always_inline_attr.cpp
+++ clang/test/lab1/durandin_vladimir/always_inline_attr.cpp
@@ -1,21 +1,23 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/AlwaysInlineAttributePlugin%pluginext
-// -plugin AddAlwaysInline %s 1>&1 | FileCheck %s
-
-// CHECK: __attribute__((always_inline)) int sum(int a, int b) {
-// CHECK-NEXT:   return a + b;
-// CHECK-NEXT: }
+// RUN: %clang_cc1 -load %llvmshlibdir/AlwaysInlineAttributePlugin%pluginext -plugin AddAlwaysInline %s 1>&1 | FileCheck %s --check-prefix=SUM
+// SUM: __attribute__((always_inline)) int sum(int a, int b) {
+// SUM-NEXT:   return a + b;
+// SUM-NEXT: }
 int sum(int a, int b) { return a + b; }
 
+// RUN: %clang_cc1 -load %llvmshlibdir/AlwaysInlineAttributePlugin%pluginext -plugin AddAlwaysInline %s 1>&1 | FileCheck %s --check-prefix=EMPTY
+// EMPTY: __attribute__((always_inline)) void checkEmpty() {
+// EMPTY-NEXT: }
 void checkEmpty() {}
 
-// CHECK: int min_nested(int a, int b) {
-// CHECK-NEXT:     {
-// CHECK-NEXT:         if (a < b) {
-// CHECK-NEXT:             return a;
-// CHECK-NEXT:         } else
-// CHECK-NEXT:             return b;
-// CHECK-NEXT:     }
-// CHECK-NEXT: }
+// RUN: %clang_cc1 -load %llvmshlibdir/AlwaysInlineAttributePlugin%pluginext -plugin AddAlwaysInline %s 1>&1 | FileCheck %s --check-prefix=MIN-NESTED
+// MIN-NESTED: int min_nested(int a, int b) {
+// MIN-NESTED-NEXT:     {
+// MIN-NESTED-NEXT:         if (a < b) {
+// MIN-NESTED-NEXT:             return a;
+// MIN-NESTED-NEXT:         } else
+// MIN-NESTED-NEXT:             return b;
+// MIN-NESTED-NEXT:     }
+// MIN-NESTED-NEXT: }
 int min_nested(int a, int b) {
   {
     if (a < b) {
@@ -25,6 +27,12 @@ int min_nested(int a, int b) {
   }
 }
 
+// CHECK: int min(int a, int b) {
+// CHECK-NEXT:     if (a < b) {
+// CHECK-NEXT:         return a;
+// CHECK-NEXT:     } else
+// CHECK-NEXT:         return b;
+// CHECK-NEXT: }
 int min(int a, int b) {
   if (a < b) {
     return a;
@@ -32,4 +40,17 @@ int min(int a, int b) {
     return b;
 }
 
-template <typename T> T min(T a, T b) { return a; }
+
+// RUN: %clang_cc1 -load %llvmshlibdir/AlwaysInlineAttributePlugin%pluginext -plugin AddAlwaysInline %s 1>&1 | FileCheck %s --check-prefix=LOOP
+// LOOP: void loop() {
+// LOOP-NEXT:     int counter = 0;
+// LOOP-NEXT:     for (int i = 0; i < 2; i++) {
+// LOOP-NEXT:         counter = i;
+// LOOP-NEXT:     }
+// LOOP-NEXT: }
+void loop() {
+  int counter = 0;
+  for (int i = 0; i < 2; i++) {
+    counter = i;
+  }
+}
