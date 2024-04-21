e36aa7a60ffcac54e8292b97866ad8223a11c0fc
Sturmannn <vladimir-ych@yandex.ru>
2024-04-21 16:56:54 +0300

    try_1

---
+++ llvm/lib/Target/X86/labs/durandin_vladimir/X86VladimirMICounter.cpp
@@ -0,0 +1,68 @@
+#include "X86.h"
+#include "X86InstrInfo.h"
+#include "X86Subtarget.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+
+#define MI_COUNTER_DESC "X86 Count number of machine instructions pass"
+#define MI_COUNTER_NAME "x86-vladimir-mi-counter"
+
+using namespace llvm;
+
+namespace {
+class X86VladimirMICounterPass : public MachineFunctionPass {
+public:
+  X86VladimirMICounterPass() : MachineFunctionPass(ID) {}
+
+  bool runOnMachineFunction(MachineFunction &MF) override;
+
+  static char ID;
+
+private:
+  StringRef getPassName() const override { return MI_COUNTER_DESC; }
+};
+} // end anonymous namespace
+
+bool X86VladimirMICounterPass::runOnMachineFunction(MachineFunction &MF) {
+  const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+  DebugLoc DL3 = MF.front().begin()->getDebugLoc();
+
+  // Create a new virtual register
+  unsigned icReg = MF.getRegInfo().createVirtualRegister(&X86::GR64RegClass);
+
+  // Check if global variable 'ic' exists
+  Module &M = *MF.getFunction().getParent();
+  GlobalVariable *gvar = M.getGlobalVariable("ic");
+
+  // If 'ic' does not exist, create it
+  if (!gvar) {
+    LLVMContext &context = M.getContext();
+    gvar = new GlobalVariable(M, IntegerType::get(context, 64), false,
+                              GlobalValue::ExternalLinkage, nullptr, "ic");
+    gvar->setAlignment(Align(8));
+  }
+
+  for (auto &MBB : MF) {
+    unsigned count = 0;
+    for (auto &MI : MBB) {
+      if (!MI.isDebugInstr())
+        ++count;
+    }
+
+    // Update the counter
+    BuildMI(MBB, MBB.getFirstTerminator(), DL3, TII->get(X86::ADD64ri8), icReg)
+        .addReg(icReg)
+        .addImm(count);
+  }
+
+  // Write to global variable ic
+  BuildMI(MF.back(), MF.back().begin(), DL3, TII->get(X86::MOV64mr))
+      .addReg(icReg)
+      .addExternalSymbol("ic");
+
+  return true;
+}
+
+char X86VladimirMICounterPass::ID = 0;
+static RegisterPass<X86VladimirMICounterPass> X(MI_COUNTER_NAME,
+                                                MI_COUNTER_DESC, false, false);
